61d96567dfac6e97c67f45bf57abdb9e597d4883
Oleksandr <sanyalisnyak@gmail.com>
2025-06-13 06:40:31 +0300

    Added calculator and week day homework

---
+++ homework4.1/homework4.1/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("enter numbers day : ");
+short operation = Convert.ToInt16(Console.ReadLine());
+
+switch (operation)
+{
+    case 1:
+        Console.WriteLine("1 - Monday");
+        break;
+
+    case 2:
+        Console.WriteLine("2 - Tuesday");
+        break;
+
+    case 3:
+        Console.WriteLine("3 - Wednesday");
+        break;
+    case 4:
+        Console.WriteLine("4 - Thursday");
+        break;
+    case 5:
+        Console.WriteLine("5 - Friday");
+        break;
+    case 6:
+        Console.WriteLine("6 - Saturday");
+        break;
+    case 7:
+        Console.WriteLine("7 - Sunday");
+        break;
+
+    default:
+        Console.WriteLine("wrong operation");
+        break;
+}
---
+++ homework4.2/homework4.2/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter operation");
+Console.WriteLine("1 - addition");
+Console.WriteLine("2 - subtraction");
+Console.WriteLine("3 - multiplication");
+Console.WriteLine("4 - division");
+
+short operation = Convert.ToInt16(Console.ReadLine());
+long a = 0;
+long b = 0;
+
+if (operation >= 1 && operation <= 4)
+{
+    Console.WriteLine("enter two numbers : ");
+    a = Convert.ToInt64(Console.ReadLine());
+    b = Convert.ToInt64(Console.ReadLine());
+}
+
+switch (operation)
+{
+    case 1:
+        Console.WriteLine($"1 - addition : {a + b}");
+        break;
+
+    case 2:
+        Console.WriteLine($"2 - subtraction : {a - b}");
+        break;
+
+    case 3:
+        Console.WriteLine($"3 - multiplication : {a * b}");
+        break;
+    case 4:
+        Console.WriteLine($"4 - division : {a / b}");
+        break;
+    default:
+        Console.WriteLine("wrong operation");
+        break;
+
+}
